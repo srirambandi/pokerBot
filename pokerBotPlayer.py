7027470ca1f727a6f55c86ae93ddf438508ba114
Mason duBoef <mduboef@gmail.com>
2025-03-31 18:52:22 -0400

    Identify all hands but straightflush & royalflush

--- pokerBotPlayer.py
+++ pokerBotPlayer.py
@@ -5,11 +5,141 @@ import pprint
 
 class PokerBotPlayer(BasePokerPlayer):
 
+  # for pairs, three of a kind & four of a kind
+  # 1 if it exists
+  # 0 if all cards are uncovered and it does not exist
+  # TODO if not there but cards are uncovered, return the probability
+  def haveOfAKind(self, cards, quantity):   # quantity = 2 for pairs, 3 for three of a kind, 4 for four of a kind
+    if len(cards) < quantity:
+      return 0.0
+    
+    for i in range(len(cards)):
+      count = 1
+      for j in range(i+1, len(cards)):
+        if cards[i][1] == cards[j][1]:
+          count += 1
+      if count == quantity:
+        return 1.0
+    return 0.0
+  
+
+  # 1 if we have two pair
+  # 0 if all cards are uncovered and no two pair
+  # TODO if no two pair rn but cards are covered, return the probability
+  def haveTwoPair(self, cards):
+    if len(cards) < 4:
+      return 0.0
+    
+    pairCount = 0
+    for i in range(len(cards)):
+      for j in range(i+1, len(cards)):
+        if cards[i][1] == cards[j][1]:
+          pairCount += 1
+          break
+
+    if pairCount == 2:
+      return 1.0
+    else:
+      return 0.0
+    
+
+  # 1 if we have a straight
+  # 0 if all cards are uncovered and no straight
+  # TODO if no straight rn but cards are covered, return the probability
+  def haveStraight(self, cards, valueDict):
+    if len(cards) < 5:
+      return 0.0
+    
+    # sort cards by value
+    cards.sort(key=lambda x: valueDict[x[1]])
+
+    streak = 1
+    for i in range(len(cards)-4):
+      for j in range(i+1, len(cards)):
+        if valueDict[cards[i+1][1]] == valueDict[cards[i][1]] + 1:
+          streak += 1
+          if streak == 5:
+            return 1.0
+        else:
+          streak = 1
+    return 0.0
+  
+
+  # 1 if we have a flush
+  # 0 if all cards are uncovered and no flush
+  # TODO if no flush rn but cards are covered, return the probability
+  def haveFlush(self, cards):
+    if len(cards) < 5:
+      return 0.0
+    
+    hearts = 0
+    diamonds = 0
+    clubs = 0
+    spades = 0
+    for i in range(len(cards)):
+      if cards[i][0] == "H":
+        hearts += 1
+      elif cards[i][0] == "D":
+        diamonds += 1
+      elif cards[i][0] == "C":
+        clubs += 1
+      elif cards[i][0] == "S":
+        spades += 1
+
+    if hearts >= 5 or diamonds >= 5 or clubs >= 5 or spades >= 5:
+      return 1.0
+
+  
+  # 1 if we have a full house
+  # 0 if all cards are uncovered and no full house
+  # TODO if no full house rn but cards are covered, return the probability
+  def haveFullHouse(self, cards):
+    if len(cards) < 5:
+      return 0.0
+    
+    # find three of a kind
+    threeRank = None
+    for i in range(len(cards)):
+      count = 1
+      for j in range(i+1, len(cards)):
+        if cards[i][1] == cards[j][1]:
+          count += 1
+      if count >= 3:
+        threeRank = cards[i][1]
+
+    # fail if no three of a kind
+    if threeRank is None:
+      return 0.0
+    
+    # find pair separate from the three of a kind
+    for i in range(len(cards)):
+      if cards[i][1] == threeRank:
+        continue
+      count = 1
+      for j in range(i+1, len(cards)):
+        if cards[i][1] == cards[j][1]:
+          count += 1
+      if count >= 3:
+        threeRank = cards[i][1]
+    
+    return 0.0
+    
+  # TODO implement straight flush
+  def haveStraightFlush(self, cards):
+    return 0.0
+  
+  # TODO implement royal flush
+  def haveRoyalFlush(self, cards):
+    return 0.0
+
+
+
   # passes action onto poker engine
   # currently always raises if possible, otherwise calls
   def declare_action(self, valid_actions, hole_card, round_state):
 
-    # print(f"CARDS:\t{hole_card}")
+    print(f"CARDS:\t{hole_card}")
+    # print(f"ROUND_STATE:\t{round_state}")
 
     # sum card values to determine if we have high, mid, or low cards
     valueDict = {
@@ -24,6 +154,29 @@ class PokerBotPlayer(BasePokerPlayer):
     val2 = valueDict[rank2]
     totalVal = val1 + val2
 
+    allCards = hole_card + round_state["community_card"]
+
+    # find probabilities of different hands
+    pair = self.haveOfAKind(allCards, 2)
+    three = self.haveOfAKind(allCards, 3)
+    four = self.haveOfAKind(allCards, 4)
+    twoPair = self.haveTwoPair(allCards)
+    straight = self.haveStraight(allCards, valueDict)
+    flush = self.haveFlush(allCards)
+    fullHouse = self.haveFullHouse(allCards)
+    straightFlush = 0.0
+    royalFlush = 0.0
+
+    # find probabilities of different hands using only community cards
+    pairC = self.haveOfAKind(round_state["community_card"], 2)
+    threeC = self.haveOfAKind(round_state["community_card"], 3)
+    fourC = self.haveOfAKind(round_state["community_card"], 4)
+    twoPairC = self.haveTwoPair(round_state["community_card"])
+    straightC = self.haveStraight(round_state["community_card"], valueDict)
+    flushC = self.haveFlush(round_state["community_card"])
+    fullHouseC = self.haveFullHouse(round_state["community_card"])
+    straightFlushC = 0.0
+    royalFlushC = 0.0
 
     # totalVal: 21-28 RAISE (if possible)
     if totalVal >= 21:
